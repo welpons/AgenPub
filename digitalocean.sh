2da19d691f6fb33e8dc7f41e7edf0f59f695aa7f
Felix Pons <welpons@gmail.com>
2020-11-12 16:11:25 +0100

    updating heroku config

--- digitalocean.sh
+++ digitalocean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script is executed when the app is deployed using digitalocean tool
-pwd=$(pwd)
-ln -s $pwd/public/index.php $pwd/index.php
-mv $pwd/public/.htaccess $pwd
+# pwd=$(pwd)
+# ln -s $pwd/public/index.php $pwd/index.php
+# mv $pwd/public/.htaccess $pwd
 
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,14 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class DefaultController
 {
+    private $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
+
     public function index(): Response
     {
+        $env = $this->params->get('your_variable');
 
         return new Response(
-            '<html><body><h1>Main Page</h1><p><a href="article/number">Article</a></p></body></html>'
+            sprintf('<html><body><h1>Main Page: %s</h1><p><a href="article/number">Article</a></p></body></html>', $env)
         );
     }
 }
